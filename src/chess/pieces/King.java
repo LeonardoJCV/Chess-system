23c0c48c0eb672e56775fe24d7afea0494702f96
Leonardo Vieira <leonardojcv@Outlook.com>
2023-08-10 18:29:11 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -15,6 +15,9 @@ public class King extends ChessPiece{
 		return "♔";
 	}
 
-	
-	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][]mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
